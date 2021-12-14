8e6408b7b961534458eddf6135c2547bd74e65a0
Paul Calzado <pafcalzado@gmail.com>
2021-12-14 15:30:43 +0800

    Add playRound function

--- index.js
+++ index.js
@@ -7,3 +7,31 @@ const CHOICES = [ROCK, PAPER, SCISSORS];
 function computerPlay() {
 	return CHOICES[Math.floor(Math.random() * CHOICES.length)];
 }
+
+function playRound(playerSelection, computerSelection) {
+	const playerChoice = playerSelection.toLowerCase();
+
+	if (playerChoice === computerSelection.toLowerCase()) {
+		return `It's a Draw! You both chose ${computerSelection}`;
+	} else if (
+		playerChoice === ROCK.toLowerCase() &&
+		computerSelection === PAPER
+	) {
+		return 'You Lose! Paper beats Rock';
+	} else if (
+		playerChoice === PAPER.toLowerCase() &&
+		computerSelection === SCISSORS
+	) {
+		return 'You Lose! Scissors beats Paper';
+	} else if (
+		playerChoice === SCISSORS.toLowerCase() &&
+		computerSelection === ROCK
+	) {
+		return 'You Lose! Scissors beats Rock';
+	} else {
+		return `You Win! ${
+			playerSelection[0].toUpperCase() +
+			playerSelection.slice(1, playerChoice.length)
+		} beats ${computerSelection}`;
+	}
+}
